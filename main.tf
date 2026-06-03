c215dadb27694e02ceaa96bcf727aaab3f372d2f
Jayden <nguyennguyenchuong.le@students.mq.edu.au>
2026-06-03 23:45:45 +1000

    Add EC2 instance configuration with Docker setup and security group updates

--- main.tf
+++ main.tf
@@ -11,14 +11,71 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
+variable "key_pair_name" {
+  description = "Name of an existing EC2 key pair for SSH access"
+  type        = string
+}
+
+data "aws_ssm_parameter" "ami" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
+resource "aws_instance" "tutorial" {
+  ami                         = data.aws_ssm_parameter.ami.value
+  instance_type               = "t3.micro"
+  key_name                    = var.key_pair_name
+  vpc_security_group_ids      = [aws_security_group.tutorial.id]
+  user_data_replace_on_change = true
+
+  user_data = <<-EOF
+    #!/bin/bash
+    set -e
+    dnf update -y
+    dnf install -y docker git
+    systemctl start docker
+    systemctl enable docker
+    git clone https://github.com/jaydennguyen296/TDJJ-COMP3050.git /opt/comp3050
+    cd /opt/comp3050
+    docker build -t comp3050-server .
+    docker run -d --name comp3050-server --restart unless-stopped \
+      -p 80:8000 \
+      -e APP_USER=admin \
+      -e APP_PASS=secret123 \
+      comp3050-server
+  EOF
+
+  tags = {
+    Name = "COMP3050-P"
+  }
+}
+
+
+output "instance_public_ip" {
+  description = "Public IP address of the instance"
+  value       = aws_instance.tutorial.public_ip
+}
+
+output "instance_id" {
+  description = "Instance ID"
+  value       = aws_instance.tutorial.id
+}
+
 resource "aws_security_group" "tutorial" {
   name        = "terraform-tutorial-sg"
-  description = "Allow SSH from anywhere"
+  description = "Allow SSH and HTTP"
 
   ingress {
     description = "SSH"
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
